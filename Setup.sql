b6fb26084be59be5f046efc57bd7c0565a4ba81e
drmodun <jan.modun.st@gmail.com>
2022-12-23 02:23:19 +0100

    initial table setup, will change and add constraints, not sure about making city table

---
+++ Setup.sql
@@ -0,0 +1,63 @@
+CREATE TABLE Accelerators(
+	AcceleratorId SERIAL PRIMARY KEY,		
+	Name VarChar(30) NOT NULL
+
+);
+CREATE TABLE PROJECTS(
+	ProjectId SERIAL PRIMARY KEY,
+	Name VarCHar(30) NOT NULL
+);
+CREATE TABLE AcceleratorProjects(
+	AccProjId Serial Primary Key,
+	AcceleratorId INT REFERENCES Accelerators(AcceleratorId) ON DELETE CASCADE,
+	ProjectId INT REFERENCES Projects(ProjectId) ON DELETE CASCADE
+);
+CREATE TABLE Countries(
+	CountryId SERIAL PRIMARY KEY,
+	Name VarChar(30) NOT NULL,
+	Population Int NOT NULL,
+	PPP decimal NOT NULL
+);
+CREATE TABLE Hotels(
+	HotelId SERIAL PRIMARY KEY,
+	name VarChar(30) NOT NULL,
+	Capacity int NOT NULL
+);
+
+CREATE TABLE Researches(
+	ResearchId SERIAL PRIMARY KEY,
+	Name VarChar(20) NOt NULL,
+	ProjectId INT REFERENCES Projects(ProjectId) ON DELETE CASCADE,
+	NumberOfQuotes Int NOT NULL,
+	ReleaseTime Timestamp NOT NULL
+);
+CREATE TABLE Scientists(
+	ScientistId SERIAL PRIMARY KEY,
+	Name VarChar(20) NOT NULL,
+	Surname VarChar(20) NOT NULL,
+	DateOfBirth Date NOT NULL,
+	Gender VarChar NOT NULL,
+	Field VarChar(10) NOT NULL,
+	Country Int REFERENCES Countries(CountryId) ON DELETE CASCADE,
+	HotelId Int REFERENCES Hotels(HotelId) ON DELETE CASCADE
+);
+
+
+CREATE TABLE ScientistsResearch(
+	ScientistId int REFERENCES Scientists(ScientistId) ON DELETE CASCADE,
+	ResearchId int REFERENCES Researches(ResearchId) ON DELETE CASCADE,
+	PRIMARY KEY (ScientistId, ResearchId)
+);
+
+/*ALTER TABLE Scientists
+//ADD CONSTRAINT DateCheck CHECK (DateOfBirth-INTERVAL '18 years'>INTERVAL '0 seconds');
+*//*RESET
+DROP TABLE ACCELERATORS CASCADE;
+DROP TABLE PROJECTS CASCADE;
+DROP TABLE HOTELS CASCADE;
+DROP TABLE COUNTRIES CASCADE;
+DROP TABLE RESEARCHES CASCADE;
+DROP TABLE SCIENTISTS CASCADE;
+DROP TABLE AcceleratorProjects CASCADE;
+DROP TABLE SCIENTistsRESEARCH CASCADE
+*/
